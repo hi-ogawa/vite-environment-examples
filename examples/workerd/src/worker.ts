371285afad85afb43349628b0e71149dcb9d5cc9
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-04-09 09:57:30 +0900

    refactor: move code

--- examples/workerd/src/worker.ts
+++ examples/workerd/src/worker.ts
@@ -17,55 +17,8 @@ export class RunnerObject implements DurableObject {
     if (url.pathname === RUNNER_INIT_PATH) {
       const { 0: webSocket, 1: webSocketReturn } = new WebSocketPair();
       (webSocket as any).accept();
-
       tinyassert(!this.#runner);
-      this.#runner = new ModuleRunner(
-        {
-          root: this.#env.__viteRoot,
-          sourcemapInterceptor: "prepareStackTrace",
-          transport: {
-            fetchModule: async (...args) => {
-              const response = await this.#env.__viteFetchModule.fetch(
-                new Request(ANY_URL, {
-                  method: "POST",
-                  body: JSON.stringify(args),
-                }),
-              );
-              tinyassert(response.ok);
-              const result = response.json();
-              return result as any;
-            },
-          },
-          hmr: {
-            connection: {
-              isReady: () => true,
-              onUpdate(callback) {
-                webSocket.addEventListener("message", (event) => {
-                  callback(JSON.parse(event.data));
-                });
-              },
-              send(messages) {
-                webSocket.send(JSON.stringify(messages));
-              },
-            },
-          },
-        },
-        {
-          runInlinedModule: async (context, transformed, id) => {
-            const codeDefinition = `'use strict';async (${Object.keys(
-              context,
-            ).join(",")})=>{{`;
-            const code = `${codeDefinition}${transformed}\n}}`;
-            const fn = this.#env.__viteUnsafeEval.eval(code, id);
-            await fn(...Object.values(context));
-            Object.freeze(context.__vite_ssr_exports__);
-          },
-          async runExternalModule(filepath) {
-            console.log("[runExternalModule]", filepath);
-            return import(filepath);
-          },
-        },
-      );
+      this.#runner = createRunner(this.#env, webSocket);
       return new Response(null, { status: 101, webSocket: webSocketReturn });
     }
 
@@ -73,4 +26,54 @@ export class RunnerObject implements DurableObject {
     const mod = await this.#runner.import(this.#env.__viteEntry);
     return mod.default.fetch(request, this.#env);
   }
+}
+
+function createRunner(env: RunnerEnv, webSocket: WebSocket) {
+  return new ModuleRunner(
+    {
+      root: env.__viteRoot,
+      sourcemapInterceptor: "prepareStackTrace",
+      transport: {
+        fetchModule: async (...args) => {
+          const response = await env.__viteFetchModule.fetch(
+            new Request(ANY_URL, {
+              method: "POST",
+              body: JSON.stringify(args),
+            }),
+          );
+          tinyassert(response.ok);
+          const result = response.json();
+          return result as any;
+        },
+      },
+      hmr: {
+        connection: {
+          isReady: () => true,
+          onUpdate(callback) {
+            webSocket.addEventListener("message", (event) => {
+              callback(JSON.parse(event.data));
+            });
+          },
+          send(messages) {
+            webSocket.send(JSON.stringify(messages));
+          },
+        },
+      },
+    },
+    {
+      runInlinedModule: async (context, transformed, id) => {
+        const codeDefinition = `'use strict';async (${Object.keys(context).join(
+          ",",
+        )})=>{{`;
+        const code = `${codeDefinition}${transformed}\n}}`;
+        const fn = env.__viteUnsafeEval.eval(code, id);
+        await fn(...Object.values(context));
+        Object.freeze(context.__vite_ssr_exports__);
+      },
+      async runExternalModule(filepath) {
+        console.log("[runExternalModule]", filepath);
+        return import(filepath);
+      },
+    },
+  );
 }
