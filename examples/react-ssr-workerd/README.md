4ef7e23e068a9bc194cfd9974c80f6bea12e5158
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-04-09 13:49:54 +0900

    chore: setup example

---
+++ examples/react-ssr-workerd/README.md
@@ -0,0 +1,18 @@
+# react-ssr
+
+https://vite-environment-examples-react-ssr.vercel.app
+
+```sh
+pnpm dev
+pnpm build
+pnpm preview
+
+pnpm vc-build
+pnpm vc-release
+```
+
+## todo
+
+- [x] basic dev
+- [x] basic build / preview
+- [x] dev hmr
---
+++ examples/react-ssr-workerd/src/entry-server.tsx
@@ -0,0 +1,9 @@
+import ReactDomServer from "react-dom/server.edge";
+import Page from "./routes/page";
+
+export async function handler(_req: Request) {
+  const ssrHtml = ReactDomServer.renderToString(<Page />);
+  let html = (await import("virtual:index-html")).default;
+  html = html.replace(/<body>/, `<body><div id="root">${ssrHtml}</div>`);
+  return new Response(html, { headers: { "content-type": "text/html" } });
+}
