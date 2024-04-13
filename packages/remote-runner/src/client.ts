5db62577ffe7c0fa06fbb9852d500a8b924cba4e
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-04-13 19:13:57 +0900

    wip

---
+++ packages/remote-runner/src/client.ts
@@ -0,0 +1,14 @@
+// https://github.com/cloudflare/workers-sdk/blame/ad1d056cb2710b790ff5d4532e9f694e099c27e2/packages/miniflare/src/plugins/core/proxy/client.ts
+
+export class ProxyClient {
+  constructor() {}
+}
+
+// proxy only get/apply?
+// everything async
+
+// special names
+//   global
+//   import
+// primitives
+// function
---
+++ packages/remote-runner/src/server.ts
@@ -0,0 +1,7 @@
+// https://github.com/cloudflare/workers-sdk/blame/ad1d056cb2710b790ff5d4532e9f694e099c27e2/packages/miniflare/src/workers/core/proxy.worker.ts#
+
+export class ProxyServer {
+  heap = new Map<number, unknown>();
+
+  constructor() {}
+}
