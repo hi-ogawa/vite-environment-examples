7ae057463e6f52867a7f35652e669ff05c507727
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-04-13 11:55:48 +0900

    fix: fix array join

--- examples/react-server/vite.config.ts
+++ examples/react-server/vite.config.ts
@@ -193,7 +193,7 @@ function vitePluginUseClient(): PluginOption {
       tinyassert(this.environment?.mode === "build");
       return [
         `export default {`,
-        ...Array(manager.clientReferences).map(
+        ...Array.from(manager.clientReferences).map(
           (id) => `"${id}": () => import("${id}"),`,
         ),
         `}`,
