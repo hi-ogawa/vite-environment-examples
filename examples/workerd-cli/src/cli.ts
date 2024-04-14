c5ceae5480831ad6292430655b8e210c71e18213
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-04-15 00:09:30 +0900

    chore: no virtual set env

--- examples/workerd-cli/src/cli.ts
+++ examples/workerd-cli/src/cli.ts
@@ -26,26 +26,6 @@ async function main() {
           return;
         },
       },
-      {
-        name: "virtual-workerd-env",
-        resolveId(source, _importer, _options) {
-          if (source === "virtual:workerd-env") {
-            return "\0" + source;
-          }
-          return;
-        },
-        load(id, _options) {
-          if (id === "\0virtual:workerd-env") {
-            return `\
-              export let env;
-              export function _setEnv(_env) {
-                env = _env;
-              }
-            `;
-          }
-          return;
-        },
-      },
     ],
     environments: {
       workerd: {
@@ -68,25 +48,19 @@ async function main() {
   });
   const devEnv = server.environments["workerd"] as WorkerdDevEnvironment;
 
-  // expose bindings via virtual module
-  await devEnv.api.eval("virtual:workerd-env", async (ctx) => {
-    ctx.exports["_setEnv"](ctx.env);
-  });
-
   // evaluate command via virtual module
   async function evaluate(cmd: string) {
     if (!cmd.includes("return")) {
       cmd = `return ${cmd}`;
     }
     const entrySource = `
-      import { env } from "virtual:workerd-env";
-      export default async function() { ${cmd} };
+      export default async function(env) { ${cmd} };
     `;
     const entry = "virtual:repl/" + encodeURI(entrySource);
     await devEnv.api.eval(
       entry,
       async function (ctx) {
-        const result = await ctx.exports["default"]();
+        const result = await ctx.exports["default"](ctx.env);
         if (typeof result !== "undefined") {
           console.log(result);
         }
