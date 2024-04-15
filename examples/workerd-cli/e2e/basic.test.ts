68e71588dd3bacee2b42430f80a99b233aa2eb34
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-04-15 11:07:10 +0900

    refactor(workerd) implement repl with `dispatchFetch` (#27)
    * refactor(workerd): serialization
    
    * chore: unused
    
    * refactor(workerd): repl via dispatchFetch
    
    * chore: revert unused
    
    * test: more test
    
    * chore: tweak
    
    * chore: comment

--- examples/workerd-cli/e2e/basic.test.ts
+++ examples/workerd-cli/e2e/basic.test.ts
@@ -2,13 +2,17 @@ import test from "node:test";
 import childProcess from "node:child_process";
 
 test("basic", async () => {
-  using proc = childProcess.spawn("pnpm", ["cli"]);
+  using proc = childProcess.spawn("pnpm", ["-s", "cli"]);
   const helper = createProcessHelper(proc);
   await helper.waitFor((out) => out.includes("[mf:inf] Ready"));
   proc.stdin.write(`env\n`);
   await helper.waitFor((out) => out.includes("{ kv: KvNamespace {} }"));
   proc.stdin.write(`env.kv.list()\n`);
   await helper.waitFor((out) => out.includes("{ keys: []"));
+  proc.stdin.write(`(await import("/package.json")).name\n`);
+  await helper.waitFor((out) =>
+    out.includes("@hiogawa/vite-environment-examples-workerd-cli"),
+  );
 });
 
 function createProcessHelper(
@@ -24,7 +28,7 @@ function createProcessHelper(
   });
 
   async function waitFor(predicate: (stdout: string) => boolean) {
-    return new Promise<void>((resolve) => {
+    const promise = new Promise<void>((resolve) => {
       const listener = () => {
         if (predicate(stdout)) {
           resolve();
@@ -33,6 +37,10 @@ function createProcessHelper(
       };
       listeners.add(listener);
     });
+    const timeout = sleep(5000).then(() => {
+      throw new Error("waitFor timeout", { cause: stdout });
+    });
+    return Promise.race([promise, timeout]);
   }
 
   return {
@@ -41,4 +49,8 @@ function createProcessHelper(
     },
     waitFor,
   };
+}
+
+function sleep(ms: number) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
 }
