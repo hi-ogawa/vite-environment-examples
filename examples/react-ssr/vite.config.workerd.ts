54f6f93a5a1ab4777ac9958b1b011890420ca6cb
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-04-15 16:35:35 +0900

    chore: workaround optimizeDeps.include

--- examples/react-ssr/vite.config.workerd.ts
+++ examples/react-ssr/vite.config.workerd.ts
@@ -34,6 +34,8 @@ export default defineConfig((_env) => ({
             "react/jsx-runtime",
             "react/jsx-dev-runtime",
             "react-dom/server.edge",
+            "seroval",
+            "seroval-plugins/web",
           ],
         },
       },
