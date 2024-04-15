065afa86016a32be8322f0129c47bc340d28e983
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-04-15 13:37:42 +0900

    test: unref timeout

--- examples/workerd-cli/e2e/basic.test.ts
+++ examples/workerd-cli/e2e/basic.test.ts
@@ -52,5 +52,5 @@ function createProcessHelper(
 }
 
 function sleep(ms: number) {
-  return new Promise((resolve) => setTimeout(resolve, ms));
+  return new Promise((resolve) => setTimeout(resolve, ms).unref());
 }
