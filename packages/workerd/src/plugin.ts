408c6bdc474a62d5c551d366f8eef692b653261f
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-04-15 13:21:46 +0900

    chore: minor

--- packages/workerd/src/plugin.ts
+++ packages/workerd/src/plugin.ts
@@ -193,14 +193,13 @@ export async function createWorkerdDevEnvironment(
         }),
         body: request.body as any,
         redirect: "manual",
-        // @ts-ignore
+        // @ts-ignore undici
         duplex: "half",
       });
-      const res_ = res as any as Response; // fix web/undici types
-      return new Response(res_.body, {
+      return new Response(res.body, {
         status: res.status,
         statusText: res.statusText,
-        headers: res_.headers,
+        headers: res.headers,
       });
     },
 
