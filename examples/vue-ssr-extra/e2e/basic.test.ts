5c15bf46dc7337351e9711761dbe5fb3e2e75774
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-04-17 18:17:51 +0900

    test: e2e

--- examples/vue-ssr-extra/e2e/basic.test.ts
+++ examples/vue-ssr-extra/e2e/basic.test.ts
@@ -2,9 +2,36 @@ import { test, expect } from "@playwright/test";
 
 test("basic", async ({ page }) => {
   const res = await page.goto("/");
-  expect(await res?.text()).toContain("hydrated: false");
-  await expect(page.locator("#root")).toContainText("hydrated: true");
-  await expect(page.locator("#root")).toContainText("Count: 0");
+  expect(await res?.text()).toContain("mounted: 0");
+
+  // client counter
+  await page.getByText("mounted: 1").click();
+  await page.getByRole("link", { name: "Counter (client)" }).click();
+  await page.waitForURL("/client");
+
+  await page.getByText("Client Counter: 0").click();
   await page.getByRole("button", { name: "+" }).click();
-  await expect(page.locator("#root")).toContainText("Count: 1");
+  await page.getByText("Client Counter: 1").click();
+  await page.getByRole("button", { name: "-" }).click();
+  await page.getByText("Client Counter: 0").click();
+});
+
+test("server action", async ({ page }) => {
+  const res1 = await page.goto("/");
+  expect(await res1?.text()).toContain("mounted: 0");
+
+  // server counter
+  await page.getByText("mounted: 1").click();
+  await page.getByRole("link", { name: "Counter (server)" }).click();
+  await page.waitForURL("/server");
+  await page.getByText("Server Counter: 0").click();
+  await page.getByRole("button", { name: "+" }).click();
+  await page.getByText("Server Counter: 1").click();
+
+  // reload
+  const res2 = await page.reload();
+  expect(await res2?.text()).toContain("Server Counter: 1");
+  await page.getByText("mounted: 1").click();
+  await page.getByRole("button", { name: "-" }).click();
+  await page.getByText("Server Counter: 0").click();
 });
--- examples/vue-ssr-extra/playwright.config.ts
+++ examples/vue-ssr-extra/playwright.config.ts
@@ -2,10 +2,8 @@ import { defineConfig, devices } from "@playwright/test";
 
 const port = Number(process.env["E2E_PORT"] || 6174);
 const command = process.env["E2E_PREVIEW"]
-  ? `pnpm preview --port ${port} --strict-port`
-  : process.env["E2E_WORKERD"]
-    ? `pnpm dev-workerd --port ${port} --strict-port`
-    : `pnpm dev --port ${port} --strict-port`;
+  ? `pnpm preview --port ${port}`
+  : `pnpm dev --port ${port} --strict-port`;
 
 export default defineConfig({
   testDir: "e2e",
