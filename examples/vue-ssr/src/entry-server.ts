e1530990001f65eab3b7ff8d35d94aa946840c96
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-04-17 11:41:12 +0900

    chore: ssr stream

--- examples/vue-ssr/src/entry-server.ts
+++ examples/vue-ssr/src/entry-server.ts
@@ -1,11 +1,25 @@
-import { renderToString } from "vue/server-renderer";
+import { renderToWebStream } from "vue/server-renderer";
 import { createSSRApp } from "vue";
 import Page from "./routes/page.vue";
 
 export async function handler(_req: Request) {
   const app = createSSRApp(Page);
-  const ssrHtml = await renderToString(app);
-  let html = (await import("virtual:index-html")).default;
-  html = html.replace(/<body>/, `<body><div id="root">${ssrHtml}</div>`);
-  return new Response(html, { headers: { "content-type": "text/html" } });
+  const ssrStream = renderToWebStream(app);
+  const html = (await import("virtual:index-html")).default;
+  const htmlStream = ssrStream.pipeThrough(injectSsr(html));
+  return new Response(htmlStream, { headers: { "content-type": "text/html" } });
+}
+
+function injectSsr(html: string) {
+  let [pre, post] = html.split("<body>") as [string, string];
+  pre = pre + `<body><div id="root">`;
+  post = `</div>` + post;
+  return new TransformStream<Uint8Array, Uint8Array>({
+    start(controller) {
+      controller.enqueue(new TextEncoder().encode(pre));
+    },
+    flush(controller) {
+      controller.enqueue(new TextEncoder().encode(post));
+    },
+  });
 }
