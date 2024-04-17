9924bb9441a44709e5921008d3a45c419dfef557
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-04-17 11:02:59 +0900

    chore: tweak

--- examples/vue-ssr/src/entry-server.tsx
+++ examples/vue-ssr/src/entry-server.tsx
@@ -6,6 +6,6 @@ export async function handler(_req: Request) {
   const app = createSSRApp(Page);
   const ssrHtml = await renderToString(app);
   let html = (await import("virtual:index-html")).default;
-  html = html.replace(/<div id="root">/, `<div id="root">${ssrHtml}</div>`);
+  html = html.replace(/<body>/, `<body><div id="root">${ssrHtml}</div>`);
   return new Response(html, { headers: { "content-type": "text/html" } });
 }
