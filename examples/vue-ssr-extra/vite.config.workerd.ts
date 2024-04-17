158c10cf0c31a5012fda6f212140d554e5a250dd
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-04-17 17:36:01 +0900

    chore: kvPersist

--- examples/vue-ssr-extra/vite.config.workerd.ts
+++ examples/vue-ssr-extra/vite.config.workerd.ts
@@ -15,6 +15,7 @@ export default defineConfig((_env) => ({
       entry: "/src/adapters/workerd.ts",
       miniflare: {
         log: new Log(),
+        kvPersist: true,
       },
       wrangler: {
         configPath: "./wrangler.toml",
