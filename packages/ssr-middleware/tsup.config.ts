13ef1dff5a0c0bde1fb7f832ab66659c6b0402c3
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-04-22 15:06:36 +0900

    fix: fix tsup

--- packages/ssr-middleware/tsup.config.ts
+++ packages/ssr-middleware/tsup.config.ts
@@ -2,16 +2,9 @@ import { defineConfig } from "tsup";
 
 export default [
   defineConfig({
-    entry: ["src/worker.ts"],
-    format: ["esm"],
-    platform: "browser",
-    noExternal: [/.*/],
-  }),
-  defineConfig({
     entry: ["src/index.ts"],
     format: ["esm"],
     platform: "node",
     dts: true,
-    external: ["vite", "miniflare"],
   }),
 ];
