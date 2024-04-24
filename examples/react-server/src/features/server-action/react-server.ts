982128493dfe309290ed47e7420bb14ab4916fda
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-04-24 17:23:22 +0900

    wip: decodeAction/decodeFormState

--- examples/react-server/src/features/server-action/react-server.ts
+++ examples/react-server/src/features/server-action/react-server.ts
@@ -1,5 +1,6 @@
 import reactServerDomWebpack from "react-server-dom-webpack/server.edge";
 import { tinyassert } from "@hiogawa/utils";
+import type { BundlerConfig, ImportManifestEntry } from "../../types";
 
 export function registerServerReference(
   action: Function,
@@ -11,15 +12,35 @@ export function registerServerReference(
 
 export async function serverActionHandler({ request }: { request: Request }) {
   const url = new URL(request.url);
-  const contentType = request.headers.get("content-type");
-  const body = contentType?.startsWith("multipart/form-data")
-    ? await request.formData()
-    : await request.text();
-  const args = await reactServerDomWebpack.decodeReply(body);
-  const actionId = url.searchParams.get("__action_id");
-  tinyassert(actionId);
-  const action = await importServerAction(actionId);
-  return await action(...args);
+  let boundAction: Function;
+  if (url.searchParams.has("__stream")) {
+    // client stream request
+    const contentType = request.headers.get("content-type");
+    const body = contentType?.startsWith("multipart/form-data")
+      ? await request.formData()
+      : await request.text();
+    const args = await reactServerDomWebpack.decodeReply(body);
+    const actionId = url.searchParams.get("__action_id");
+    tinyassert(actionId);
+    const action = await importServerAction(actionId);
+    boundAction = () => action(...args);
+  } else {
+    // progressive enhancement
+    const formData = await request.formData();
+    const decodedAction = await reactServerDomWebpack.decodeAction(
+      formData,
+      createActionBundlerConfig(),
+    );
+    boundAction = async () => {
+      const result = await decodedAction();
+      const formState = await reactServerDomWebpack.decodeFormState(
+        result,
+        formData,
+      );
+      return formState;
+    };
+  }
+  return boundAction();
 }
 
 async function importServerReference(id: string): Promise<unknown> {
@@ -37,4 +58,23 @@ async function importServerAction(id: string): Promise<Function> {
   const [file, name] = id.split("#") as [string, string];
   const mod: any = await importServerReference(file);
   return mod[name];
+}
+
+function createActionBundlerConfig(): BundlerConfig {
+  return new Proxy(
+    {},
+    {
+      get(_target, $$id, _receiver) {
+        tinyassert(typeof $$id === "string");
+        let [id, name] = $$id.split("#");
+        tinyassert(id);
+        tinyassert(name);
+        return {
+          id,
+          name,
+          chunks: [],
+        } satisfies ImportManifestEntry;
+      },
+    },
+  );
 }
