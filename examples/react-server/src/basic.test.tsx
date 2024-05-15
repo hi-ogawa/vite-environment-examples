ca29d07bc0dcfe1d1321ce545a3d3e346392d650
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-05-15 18:23:23 +0900

    test: update

--- examples/react-server/src/basic.test.tsx
+++ examples/react-server/src/basic.test.tsx
@@ -52,7 +52,7 @@ async function testRender(container: reactDomClient.Container, page: string) {
 }
 
 test("basic", async () => {
-  await testRender(document, "/src/routes/page");
+  await testRender(document.body, "/src/routes/page");
   await vi.waitUntil(() =>
     document.body.querySelector(`[data-hydrated="true"]`),
   );
