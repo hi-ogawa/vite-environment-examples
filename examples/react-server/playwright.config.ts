390b3638bbc5e4d1e067bf92f09bcaf31c759ae6
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-05-16 12:01:23 +0900

    chore: tweak

--- examples/react-server/playwright.config.ts
+++ examples/react-server/playwright.config.ts
@@ -3,7 +3,9 @@ import { defineConfig, devices } from "@playwright/test";
 const port = Number(process.env["E2E_PORT"] || 6174);
 const isPreview = Boolean(process.env["E2E_PREVIEW"]);
 const command = isPreview
-  ? `pnpm cf-preview --port ${port}`
+  ? process.env["E2E_CF"]
+    ? `pnpm cf-preview --port ${port}`
+    : `pnpm preview --port ${port} --strict-port`
   : `pnpm dev --port ${port} --strict-port`;
 
 export default defineConfig({
