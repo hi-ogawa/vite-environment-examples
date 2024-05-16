46d6b17fa646e9f71f5b4ef3f9b22bbb07c64b70
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-05-16 11:58:35 +0900

    test(react-server): test cloudflare build

--- examples/react-server/playwright.config.ts
+++ examples/react-server/playwright.config.ts
@@ -3,7 +3,7 @@ import { defineConfig, devices } from "@playwright/test";
 const port = Number(process.env["E2E_PORT"] || 6174);
 const isPreview = Boolean(process.env["E2E_PREVIEW"]);
 const command = isPreview
-  ? `pnpm preview --port ${port} --strict-port`
+  ? `pnpm cf-preview --port ${port}`
   : `pnpm dev --port ${port} --strict-port`;
 
 export default defineConfig({
