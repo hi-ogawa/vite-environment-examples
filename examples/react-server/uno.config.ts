ba22d6ddf23fc642ac34aec1398c070d51103cda
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-05-16 11:42:50 +0900

    chore: inline uno.config.ts

--- examples/react-server/uno.config.ts
+++
@@ -1,6 +0,0 @@
-import { defineConfig, presetUno, transformerVariantGroup } from "unocss";
-
-export default defineConfig({
-  presets: [presetUno()],
-  transformers: [transformerVariantGroup()],
-});
