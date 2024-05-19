d95e7f352b69c6f2f2838fc85df9cb5ae553c45d
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-05-19 13:24:02 +0900

    chore: debug sourcemap

--- examples/react-server/src/features/server-action/plugin.test.ts
+++ examples/react-server/src/features/server-action/plugin.test.ts
@@ -1,9 +1,26 @@
+import { dirname } from "path";
+import { hashString } from "@hiogawa/utils";
+import { mkdir, writeFile } from "fs/promises";
+import type MagicString from "magic-string";
 import { describe, expect, it } from "vitest";
 import { transformServerAction2 } from "./plugin";
+
+function inlineSourceMap(output: MagicString) {
+  const code = output.toString();
+  const map = output.generateMap({ includeContent: true });
+  const encoded = Buffer.from(JSON.stringify(map), "utf-8").toString("base64");
+  return `${code}\n\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,${encoded}\n`;
+}
 
 describe(transformServerAction2, () => {
   async function testTransform(input: string) {
     const output = await transformServerAction2(input, "<id>");
+    if (output && process.env["DEBUG_SOURCEMAP"]) {
+      // open it on https://evanw.github.io/source-map-visualization
+      const filepath = `dist/debug-sourcemap/${hashString(input)}.js`;
+      await mkdir(dirname(filepath), { recursive: true });
+      await writeFile(filepath, inlineSourceMap(output));
+    }
     return output?.toString();
   }
 
