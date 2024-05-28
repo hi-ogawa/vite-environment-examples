199fa98a6185b9cf2aa2af4e1c8bee6aec3a66f6
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-05-28 21:06:41 +0900

    chore(deps): update to fix export let reference

--- examples/react-server/src/routes/action/_action.tsx
+++ examples/react-server/src/routes/action/_action.tsx
@@ -1,11 +1,6 @@
 "use server";
 
-// TODO: support `export let ...` and preserve reference;
-let count1 = 0;
-
-export function getCount1() {
-  return count1;
-}
+export let count1 = 0;
 
 export function changeCount1(formData: FormData) {
   count1 += Number(formData.get("value"));
