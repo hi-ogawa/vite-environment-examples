9b90b02d595553338b47b20837010df6e2d4f722
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-06-08 15:41:49 +0900

    chore: move repro to outside

--- examples/repro/src/thing.js
+++
@@ -1,3 +0,0 @@
-console.log("[debug:import]", import.meta.url);
-
-export const thing = { k: "v" };
--- examples/repro/test-node.js
+++
@@ -1,7 +0,0 @@
-import { fileURLToPath } from "node:url";
-
-const { getThing, myImport } = await import("./src/entry.js");
-
-const thingAbsPath = fileURLToPath(new URL("./src/thing.js", import.meta.url));
-const direct = await myImport(thingAbsPath);
-console.log(direct.thing === getThing());
