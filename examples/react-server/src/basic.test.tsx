de6f1254cd722e51e71f26400208f7948554ff53
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-08-31 16:10:08 +0900

    chore: cleanup

--- examples/react-server/src/basic.test.tsx
+++ examples/react-server/src/basic.test.tsx
@@ -51,7 +51,7 @@ async function testRender(container: reactDomClient.Container, page: string) {
   await mounted;
 }
 
-test.skip("basic", async () => {
+test("basic", async () => {
   await testRender(document.body, "/src/routes/page");
   expect(document.firstElementChild).toMatchSnapshot();
 });
