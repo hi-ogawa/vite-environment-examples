9c34a9916a29f87fa5248e029e085c473f5bb62d
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-09-25 17:47:30 +0900

    fix(workerd): fix eval filename (#111)

--- packages/workerd/src/worker.ts
+++ packages/workerd/src/worker.ts
@@ -1,5 +1,9 @@
 import { objectPickBy, tinyassert } from "@hiogawa/utils";
-import { ModuleRunner } from "vite/module-runner";
+import {
+  ModuleRunner,
+  ssrImportMetaKey,
+  ssrModuleExportsKey,
+} from "vite/module-runner";
 import {
   ANY_URL,
   type EvalFn,
@@ -107,14 +111,17 @@ function createRunner(env: RunnerEnv, webSocket: WebSocket) {
       },
     },
     {
-      runInlinedModule: async (context, transformed, id) => {
+      runInlinedModule: async (context, transformed) => {
         const codeDefinition = `'use strict';async (${Object.keys(context).join(
           ",",
         )})=>{{`;
         const code = `${codeDefinition}${transformed}\n}}`;
-        const fn = env.__viteUnsafeEval.eval(code, id);
+        const fn = env.__viteUnsafeEval.eval(
+          code,
+          context[ssrImportMetaKey].filename,
+        );
         await fn(...Object.values(context));
-        Object.freeze(context.__vite_ssr_exports__);
+        Object.freeze(context[ssrModuleExportsKey]);
       },
       async runExternalModule(filepath) {
         return import(filepath);
