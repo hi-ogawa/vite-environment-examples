bd6b759398e275a1b5cde261a2baca782abc3f4f
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-09-28 15:47:38 +0900

    test: fix

--- examples/browser-cli/README.md
+++ examples/browser-cli/README.md
@@ -5,8 +5,8 @@
 $ pnpm cli
 > window.navigator.userAgent
 Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) HeadlessChrome/127.0.6533.17 Safari/537.36
-> (await import("react-dom")).render(<div>yay</div>, document.body)
-ref: <Node>
+> (await import("react-dom/client")).createRoot(document.body).render(<div>yay</div>);
+undefined
 > document.body.innerHTML
 <div>yay</div>
 ```
--- examples/browser-cli/e2e/basic.test.ts
+++ examples/browser-cli/e2e/basic.test.ts
@@ -6,13 +6,10 @@ test("basic", async () => {
   using proc = childProcess.spawn("pnpm", ["-s", "cli"]);
   const helper = createProcessHelper(proc);
   await helper.waitFor((out) => out.includes("> "));
-  const code = [
-    `const ReactDom = await import("react-dom");`,
-    `ReactDom.render(<div style={{ color: "red" }}>yay</div>, document.body);`,
-    `document.body.innerHTML;`,
-  ].join("");
-  proc.stdin.write(code + "\n");
-  await helper.waitFor((out) =>
-    out.includes(`> <div style="color: red;">yay</div>`),
+  proc.stdin.write(
+    `(await import("react-dom/client")).createRoot(document.body).render(<div>yay</div>)\n`,
   );
+  await helper.waitFor((out) => out.includes(`undefined`));
+  proc.stdin.write("document.body.innerHTML\n");
+  await helper.waitFor((out) => out.includes(`<div>yay</div>`));
 });
