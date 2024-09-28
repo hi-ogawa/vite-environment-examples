293ac6f1c3c87fd843d06527b841b23269b86025
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-09-28 09:59:29 +0900

    test: add e2e

--- examples/web-worker-rsc/e2e/basic.test.ts
+++ examples/web-worker-rsc/e2e/basic.test.ts
@@ -3,6 +3,9 @@ import { expect, test } from "@playwright/test";
 test("basic", async ({ page }) => {
   await page.goto("/");
   await expect(page.getByTestId("worker-message")).toContainText(
-    "Rendered in web worker",
+    "[Suspense:fallback]",
+  );
+  await expect(page.getByTestId("worker-message")).toContainText(
+    "[Suspense:OK]",
   );
 });
