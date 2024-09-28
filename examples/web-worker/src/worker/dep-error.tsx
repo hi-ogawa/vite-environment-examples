78e9ead2d1863b26acc3eb919e9c8e5faf6f7f6f
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-09-28 14:26:11 +0900

    test: test module runner stacktrace on browser

---
+++ examples/web-worker/src/worker/dep-error.tsx
@@ -0,0 +1,11 @@
+export interface _SomeTs {
+  hello: "world";
+}
+
+export function depThrowError() {
+  depThrowError2();
+}
+
+function depThrowError2() {
+  throw new Error("test-stacktrace");
+}
