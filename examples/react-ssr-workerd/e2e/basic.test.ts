7e5782690c0cb5eca7a9d56140e2b198616b6a08
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-10-21 18:10:26 +0900

    test: update stack

--- examples/react-ssr-workerd/e2e/basic.test.ts
+++ examples/react-ssr-workerd/e2e/basic.test.ts
@@ -22,8 +22,8 @@ test("server error stack", async ({ request }) => {
   text = text.replaceAll(process.cwd(), "__CWD__");
   expect(text).toMatch(`\
 Error: crash ssr
-    at Module.crashSsr (__CWD__/src/crash-ssr.ts:7:9)
-    at Module.handler (__CWD__/src/entry-server.tsx:15:5)`);
+    at crashSsr (__CWD__/src/crash-ssr.ts:7:9)
+    at handler (__CWD__/src/entry-server.tsx:15:5)`);
 });
 
 test("hot custom message", async ({ request }) => {
