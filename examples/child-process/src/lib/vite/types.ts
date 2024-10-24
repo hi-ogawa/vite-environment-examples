cae70b852257ba84839544d500623afd51759300
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-10-24 14:01:52 +0900

    feat: use new module transport

--- examples/child-process/src/lib/vite/types.ts
+++ examples/child-process/src/lib/vite/types.ts
@@ -1,5 +1,12 @@
+import type { HotPayload } from "vite";
+
 export interface BridgeClientOptions {
   bridgeUrl: string;
   root: string;
+  key: string;
+}
+
+export interface InvokePayload {
+  data: HotPayload;
   key: string;
 }
