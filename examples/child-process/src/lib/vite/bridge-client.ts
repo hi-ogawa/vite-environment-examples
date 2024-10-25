f7672029d575cbd09f72a2ed01857ed48d9e690a
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-10-25 14:28:28 +0900

    chore: move code

--- examples/child-process/src/lib/vite/bridge-client.ts
+++ examples/child-process/src/lib/vite/bridge-client.ts
@@ -1,5 +1,6 @@
 import assert from "node:assert";
 import { ESModulesEvaluator, ModuleRunner } from "vite/module-runner";
+import { createSSEClient } from "./sse-client";
 import type { BridgeClientOptions } from "./types";
 
 export function createBridgeClient(options: BridgeClientOptions) {
@@ -46,77 +47,4 @@ export function createBridgeClient(options: BridgeClientOptions) {
   }
 
   return { runner, handler };
-}
-
-async function createSSEClient(
-  url: string,
-  handlers: {
-    onMessage: (payload: any) => void;
-    onDisconnection: () => void;
-  },
-) {
-  const response = await fetch(url);
-  assert(response.ok);
-  const clientId = response.headers.get("x-client-id");
-  assert(clientId);
-  assert(response.body);
-  response.body
-    .pipeThrough(new TextDecoderStream())
-    .pipeThrough(splitTransform("\n\n"))
-    .pipeTo(
-      new WritableStream({
-        write(chunk) {
-          // console.log("[client.response]", chunk);
-          if (chunk.startsWith("data: ")) {
-            const payload = JSON.parse(chunk.slice("data: ".length));
-            handlers.onMessage(payload);
-          }
-        },
-        abort(e) {
-          console.log("[client.abort]", e);
-        },
-        close() {
-          console.log("[client.close]");
-          handlers.onDisconnection();
-        },
-      }),
-    )
-    .catch((e) => {
-      console.log("[client.pipeTo.catch]", e);
-    });
-
-  return {
-    send: async (payload: unknown) => {
-      const response = await fetch(url, {
-        method: "POST",
-        body: JSON.stringify(payload),
-        headers: {
-          "x-client-id": clientId,
-        },
-      });
-      assert(response.ok);
-      const result = await response.json();
-      return result;
-    },
-  };
-}
-
-export function splitTransform(sep: string): TransformStream<string, string> {
-  let pending = "";
-  return new TransformStream({
-    transform(chunk, controller) {
-      while (true) {
-        const i = chunk.indexOf(sep);
-        if (i >= 0) {
-          pending += chunk.slice(0, i);
-          controller.enqueue(pending);
-          pending = "";
-          chunk = chunk.slice(i + sep.length);
-          continue;
-        }
-        pending += chunk;
-        break;
-      }
-    },
-  });
 }
---
+++ examples/child-process/src/lib/vite/sse-server.ts
@@ -0,0 +1,115 @@
+import assert from "node:assert";
+import type { HotChannel, HotChannelListener, HotPayload } from "vite";
+
+export function createHMRChannelSSEHandler() {
+  interface SSEClient {
+    send(payload: HotPayload): void;
+    close(): void;
+  }
+
+  const clientMap = new Map<string, SSEClient>();
+  const listenerManager = createListenerManager();
+
+  async function handler(request: Request): Promise<Response | undefined> {
+    const url = new URL(request.url);
+    if (url.pathname === "/sse") {
+      // handle `send`
+      const senderId = request.headers.get("x-client-id");
+      if (senderId) {
+        const client = clientMap.get(senderId);
+        assert(client);
+        const payload = await request.json();
+        listenerManager.handle(payload, client);
+        return Response.json({ ok: true });
+      }
+      // otherwise handle `connect`
+      let controller: ReadableStreamDefaultController<string>;
+      const stream = new ReadableStream<string>({
+        start: (controller_) => {
+          controller = controller_;
+          controller.enqueue(`:ping\n\n`);
+        },
+        cancel() {
+          clientMap.delete(clientId);
+        },
+      });
+      const pingInterval = setInterval(() => {
+        controller.enqueue(`:ping\n\n`);
+      }, 10_000);
+      const clientId = Math.random().toString(36).slice(2);
+      const client: SSEClient = {
+        send(payload) {
+          controller.enqueue(`data: ${JSON.stringify(payload)}\n\n`);
+        },
+        close() {
+          clearInterval(pingInterval);
+          controller.close();
+        },
+      };
+      clientMap.set(clientId, client);
+      return new Response(stream, {
+        headers: {
+          "x-client-id": clientId,
+          "content-type": "text/event-stream",
+          "cache-control": "no-cache",
+          connection: "keep-alive",
+        },
+      });
+    }
+    return undefined;
+  }
+
+  const channel: HotChannel = {
+    listen() {},
+    close() {
+      for (const client of clientMap.values()) {
+        client.close();
+      }
+    },
+    on: listenerManager.on,
+    off: listenerManager.off,
+    send: (payload) => {
+      for (const client of clientMap.values()) {
+        client.send(payload);
+      }
+    },
+  };
+
+  return { channel, handler };
+}
+
+// wrapper to simplify listener management
+function createListenerManager(): Pick<HotChannel, "on" | "off"> & {
+  handle: (
+    payload: HotPayload,
+    client: { send: (payload: HotPayload) => void },
+  ) => void;
+} {
+  const listerMap: Record<string, Set<HotChannelListener>> = {};
+  const getListerMap = (e: string) => (listerMap[e] ??= new Set());
+
+  return {
+    on(event: string, listener: HotChannelListener) {
+      // console.log("[channel.on]", event, listener);
+      if (event === "connection") {
+        return;
+      }
+      getListerMap(event).add(listener);
+    },
+    off(event, listener: any) {
+      // console.log("[channel.off]", event, listener);
+      if (event === "connection") {
+        return;
+      }
+      getListerMap(event).delete(listener);
+    },
+    handle(payload, client) {
+      if (payload.type === "custom") {
+        for (const lister of getListerMap(payload.event)) {
+          // TODO: type error of `payload.invoke`
+          lister(payload.data, client, payload.invoke as any);
+        }
+      }
+    },
+  };
+}
