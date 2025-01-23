b46be661bcef04720dcaa67afd3548df3bd1cf26
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2025-01-23 14:18:30 +0900

    chore: fix lint

--- examples/child-process/src/lib/ambient.d.ts
+++ examples/child-process/src/lib/ambient.d.ts
@@ -1,4 +1,2 @@
 declare const __vite_server: import("vite").ViteDevServer;
-declare const __vite_environment_rsc__: import(
-  "./vite/environment",
-).ChildProcessFetchDevEnvironment;
+declare const __vite_environment_rsc__: import("./vite/environment").ChildProcessFetchDevEnvironment;
