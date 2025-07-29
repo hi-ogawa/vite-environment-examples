2755d67346274bf7453fa094f38fc2b299216af3
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2025-07-29 12:17:49 +0900

    ci: tweak vite ecosystem ci

--- misc/vite-ecosystem-ci.sh
+++ misc/vite-ecosystem-ci.sh
@@ -6,10 +6,10 @@ pnpm -C examples/web-worker test-e2e
 pnpm -C examples/web-worker build
 pnpm -C examples/web-worker test-e2e-preview
 
-# workerd environment
-pnpm -C examples/react-ssr-workerd test-e2e
+# # workerd environment
+# pnpm -C examples/react-ssr-workerd test-e2e
 
-# react server environment
-pnpm -C examples/react-server test-e2e
-pnpm -C examples/react-server build
-pnpm -C examples/react-server test-e2e-preview
+# # react server environment
+# pnpm -C examples/react-server test-e2e
+# pnpm -C examples/react-server build
+# pnpm -C examples/react-server test-e2e-preview
