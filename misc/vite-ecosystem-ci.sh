967e1f292fc152937351bb4f5faeb7cf2ff89717
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2025-07-29 12:21:26 +0900

    chore: tweak

--- misc/vite-ecosystem-ci.sh
+++ misc/vite-ecosystem-ci.sh
@@ -5,11 +5,3 @@ set -eu -o pipefail
 pnpm -C examples/web-worker test-e2e
 pnpm -C examples/web-worker build
 pnpm -C examples/web-worker test-e2e-preview
-
-# # workerd environment
-# pnpm -C examples/react-ssr-workerd test-e2e
-
-# # react server environment
-# pnpm -C examples/react-server test-e2e
-# pnpm -C examples/react-server build
-# pnpm -C examples/react-server test-e2e-preview
